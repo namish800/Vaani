311d702a77c1f97e2aa3e4c21a0205d3ec03f0bb
namish800 <namishpruthi800@gmail.com>
2025-09-02 23:00:50 +0530

    Adds claude

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from universalagent.agents.entrypoint import create_worker_options
 from livekit import agents
+from dotenv import load_dotenv
+
+_ = load_dotenv()
 
 if __name__ == "__main__":
     worker_options = create_worker_options()
